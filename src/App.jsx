abb89ec47a97ced4eb318ff4d415a7cc27eb28db
maryam badakhshan <maryam@example.com>
2026-07-18 21:10:43 +0330

    add random sound

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Aniaml from "./components/Animal"
+
 
 function App(){
   const animals=[
@@ -7,6 +9,12 @@ function App(){
   {id:1 , Aname:"مرغ" , sound:"قد قد قدا!"},
   {id:1 , Aname:"گاو" , sound:" مموووو!"}
 ];
+  const [randomAnimal, setrandomAnimal]=useState("");
+
+  function RandomSound(){
+    const rnd= Math.floor(Math.random()*animals.length);
+    setrandomAnimal(animals[rnd])
+  }
 return (
   <div>
     <h1>لیست حیوانات</h1>
@@ -15,6 +23,10 @@ return (
       Aname={animal.Aname}
       sound={animal.sound}/>
     ))}
+    <button onClick={RandomSound}>پخش تصادفی</button>
+    {randomAnimal && (
+      <p>{randomAnimal.sound}</p>
+    )}
   </div>
 );
 }
